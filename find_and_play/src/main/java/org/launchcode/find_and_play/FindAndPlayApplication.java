2e8e45ec00bda3fa9c5b06c34495b3a565d17423
melissamurphey77 <40648706+melissamurphey77@users.noreply.github.com>
2020-04-25 15:18:10 -0500

    Uploaded base code for springboot app

---
+++ find_and_play/src/main/java/org/launchcode/find_and_play/FindAndPlayApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.find_and_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindAndPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindAndPlayApplication.class, args);
+	}
+
+}
